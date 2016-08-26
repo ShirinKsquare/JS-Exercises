177de76d2f639fd0544c62e16d255038b00913c7
Shirin Sardar <shirin.sardar@ksquarelabs.com>
2016-08-26 16:56:54 -0500

    finished exercise6 of Chapter4 related to ninja book

--- Ninja-Exercises/Chapter4-Exe/exercises.js
+++ Ninja-Exercises/Chapter4-Exe/exercises.js
@@ -1,10 +1,10 @@
-function addRec(arr){
-	console.log(arr)
-		return arr.length>1 ? arr.pop() + addRec(arr):arr[0];
+function addRec(arr) {
+    console.log(arr)
+    return arr.length > 1 ? arr.pop() + addRec(arr) : arr[0];
 }
 
-var arr=[1,3,5,7];
-var sum=addRec(arr);
+var arr = [1, 3, 5, 7];
+var sum = addRec(arr);
 console.log(sum);
 
 /* Exercise 3 */
@@ -66,36 +66,58 @@ console.log(sum);
 
 
 /*Exercise 4 */
-function dataType(){
-	var args = Array.prototype.slice.call(arguments); //change arguments to array
-	for (var i=0;i<arguments.length;i++){
-		if ( Array.isArray(args[i])) {
-			args[i]="array";
-		}else
-		args[i]=(typeof args[i]);
-	}
-		var str = args.join(', ');
-		console.log(str);
-} 
-
-/* Exercise 5 */
-function distance(){
-	if (arguments.length==4) {
-		return Math.sqrt(
-			Math.pow(arguments[0]-arguments[2],2)+Math.pow(arguments[1]-arguments[3],2)
-			);
-	}
-	if (arguments.length==6){
-		return Math.sqrt(
-			Math.pow(arguments[0]-arguments[3],2)+Math.pow(arguments[1]-arguments[4],2)+Math.pow(arguments[2]-arguments[5],2)
-			);
-	}
-	else
-	return ("there are not enough arguments or there are many arguments");
+function dataType() {
+    var args = Array.prototype.slice.call(arguments); //change arguments to array
+    for (var i = 0; i < arguments.length; i++) {
+        if (Array.isArray(args[i])) {
+            args[i] = "array";
+        } else
+            args[i] = (typeof args[i]);
+    }
+    var str = args.join(', ');
+    console.log(str);
 }
 
-console.log (distance(1,2,2,1))
-console.log(distance(1,2,1,-1,2,3))
+/* Exercise 5 */
+function distance() {
+    if (arguments.length == 4) {
+        return Math.sqrt(
+            Math.pow(arguments[0] - arguments[2], 2) + Math.pow(arguments[1] - arguments[3], 2)
+        );
+    }
+    if (arguments.length == 6) {
+        return Math.sqrt(
+            Math.pow(arguments[0] - arguments[3], 2) + Math.pow(arguments[1] - arguments[4], 2) + Math.pow(arguments[2] - arguments[5], 2)
+        );
+    } else
+        return ("there are not enough arguments or there are many arguments");
+}
 
+console.log(distance(1, 2, 2, 1))
+console.log(distance(1, 2, 1, -1, 2, 3))
 
-// /*Exercise 6 */
+/*Exercise 6 */
+
+function distance2() {
+    if ((arguments.length == 2) && (arguments[0].length == arguments[1].length)) {
+        sum = 0;
+        for (var i = 0; i < arguments[0].length - 1; i++) {
+            sum += Math.pow(arguments[0][i] - arguments[1][i], 2);
+            console.log(sum)
+            sum = Math.sqrt(sum);
+        }
+        return sum;
+    } else if (arguments.length == 4) {
+        return Math.sqrt(
+            Math.pow(arguments[0] - arguments[2], 2) + Math.pow(arguments[1] - arguments[3], 2)
+        );
+    } else if (arguments.length == 6) {
+        return Math.sqrt(
+            Math.pow(arguments[0] - arguments[3], 2) + Math.pow(arguments[1] - arguments[4], 2) + Math.pow(arguments[2] - arguments[5], 2)
+        );
+    } else
+        return "incompatible";
+}
+
+console.log(distance2(1, 2, 2, 2));
+console.log(distance2([1, 2], [2, 2, 4]));
