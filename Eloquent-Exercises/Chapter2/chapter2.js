ac7b8561cf927c1b8f068d81778aad624bff7ec1
ShirinKsquare <shirin.sardar@ksquarelabs.com>
2016-08-31 13:24:12 -0500

    Update chapter2.js

--- Eloquent-Exercises/Chapter2/chapter2.js
+++ Eloquent-Exercises/Chapter2/chapter2.js
@@ -1,3 +1,4 @@
+ <a href= "https://repl.it/CpE0/204"> here </a>
  Chapter2 Exercise1 
 ``` Javascript
  var PoundCount=1;
@@ -31,40 +32,44 @@
  }
 ```
 
-/* Chapter2 Exercise2 Version2*/
-// var count=1;
-// for (count=1;count<101;count+=1){
-// 	if (count%3==0){
-// 		console.log("Fizz")
-// 	}
-// 	else if(count%5==0 && !count%3==0){
-// 		console.log("Buzz")
-// 	}
-// 	else if (count%5==0 && count%3==0){
-// 		console.log("FizzBuzz")
-// 	}
-// 	else{
-// 		console.log(count)
-// 	}
-// }
-/* Chapter2 Exercise3 */
+ Chapter2 Exercise2 Version2
+ 
+ ```
+ var count=1;
+ for (count=1;count<101;count+=1){
+ 	if (count%3==0){
+ 		console.log("Fizz")
+ 	}
+ 	else if(count%5==0 && !count%3==0){
+ 		console.log("Buzz")
+ 	}
+ 	else if (count%5==0 && count%3==0){
+ 		console.log("FizzBuzz")
+ 	}
+ 	else{
+ 		console.log(count)
+ 	}
+ }
+ ```
+ Chapter2 Exercise3 
+```
+ var count1=1;
+ var count2=1;
+ var sum=0;
+ var result=""
 
-// var count1=1;
-// var count2=1;
-// var sum=0;
-// var result=""
-
-// for (count1=1;count1<9;count1+=1){
-// 	for (count2=1; count2<9;count2+=1){
-// 		sum=count1+count2;
-// 		if (sum%2==0){
-// 			result=result+" ";
-// 		}
-// 		else{
-// 		result=result+"#";
-// 		}
-// 	}
-//  	console.log(result);
-//  	console.log("\n");
-//  	result="";
-// }
+ for (count1=1;count1<9;count1+=1){
+ 	for (count2=1; count2<9;count2+=1){
+ 		sum=count1+count2;
+ 		if (sum%2==0){
+ 			result=result+" ";
+ 		}
+ 		else{
+ 		result=result+"#";
+ 		}
+ 	}
+  	console.log(result);
+  	console.log("\n");
+  	result="";
+ }
+```
