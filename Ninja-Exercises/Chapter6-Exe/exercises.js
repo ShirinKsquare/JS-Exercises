4e35ef551bf056d8ecd38105c7b20027c6acee49
Shirin Sardar <shirin.sardar@ksquarelabs.com>
2016-09-01 12:49:12 -0500

    added Chpater6 exercise of Ninja

--- Ninja-Exercises/Chapter6-Exe/exercises.js
+++ Ninja-Exercises/Chapter6-Exe/exercises.js
@@ -1,7 +1,34 @@
+/* Chapter6 Exercise 1 */
+
+
+function d(points) {
+
+    if (points.length == 4)
+        return (Math.sqrt(Math.pow(points[0] - points[2], 2) + Math.pow(points[1] - points[3], 2)));
+
+    else
+        return "incompatible for 2D";
+}
+
+
+//-------------------------------------------------------
+
+function shoelace(points) {
+    var sum = 0;
+    for (var i = 0; i < (points.length) / 2; i++) {
+        sum = sum + (points[i] * points[i + 3] - points[i + 1] * points[i + 2]);
+    }
+    return ((Math.abs(sum)) / 2);
+}
+
+//console.log(d([0,1,0,0]))
+//console.log(shoelace([2,1,4,5,7,8]));
+
+
+//--------------------------------------------------------
 function Shape(pEdge) {
-    // this.pEdge=pEdge;
     this.pEdge = function() {
-        console.log(this.coordinates)
+        console.log(this.coordinates);
     }
     this.fnDisplay = function() {
         console.log(this.name);
@@ -10,56 +37,73 @@ function Shape(pEdge) {
 
 function Triangle(name, coordinates) {
     this.name = name;
+    this.coordinates = coordinates;
     this.fnArea = function() {
-        //use Shoelace formula
+        return shoelace(this.coordinates);
     }
     this.fnPerimeter = function() {
 
-        //	d((this.coordinates[0],this.coordinates[1]))+d((this.coordinates[2],pEdge[3]))+d((pEdge(4,pEdge[6]))
+        return d([this.coordinates[0], this.coordinates[1], this.coordinates[2], this.coordinates[3]]) +
+            d([this.coordinates[2], this.coordinates[3], this.coordinates[4], this.coordinates[5]]) +
+            d([this.coordinates[4], this.coordinates[5], this.coordinates[0], this.coordinates[1]]);
     }
+    Shape.call(this, this.coordinates);
 
 }
 
 function Quadrilateral() {
-    this.fnArea = function() {
+    // this.fnArea=function(){
+    // 	return shoelace(this.coordinates);
 
-    }
-    this.fnPerimeter = function() {
-        //this.coordinates[0]
-        //	d((pEdge[0],pEdge[1]))+d((pEdge[2],pEdge[3]))+d((pEdge(4,pEdge[6]))
-    }
+    // }
+    // this.fnPerimeter=function(){
+
+    // 	return 	d([this.coordinates[0],this.coordinates[1],this.coordinates[2],this.coordinates[3]])+
+    // 		    d([this.coordinates[2],this.coordinates[3],this.coordinates[4],this.coordinates[5]])+
+    // 		    d([this.coordinates[4],this.coordinates[5],this.coordinates[6],this.coordinates[7]])+
+    // 		    d([this.coordinates[6],this.coordinates[7],this.coordinates[0],this.coordinates[1] ]);
+    // }
+    Shape.call(this, this.coordinates)
 }
 
 function Square(name, coords) {
     this.name = name;
     this.coordinates = coords;
+    Quadrilateral.call(this);
 }
 
-Triangle.prototype = new Shape();
-Quadrilateral.prototype = new Shape();
-Square.prototype = new Quadrilateral();
 
-// inheritence should come before creating instances
+// prototype chain
+Quadrilateral.prototype = Object.create(Shape.prototype);
+Square.prototype = Object.create(Quadrilateral.prototype);
+Triangle.prototype = Object.create(Shape.prototype);
 
-//var shape=new Shape();
-var triangle = new Triangle("name", 0, 9);
-//var quadrilateral= new Quadrilateral();
-var square = new Square("name", 0, 1, 2, 3, 4, 5);
+Quadrilateral.prototype.fnArea = function() {
+    return shoelace(this.coordinates);
 
-console.log(square.pEdge());
+}
+Quadrilateral.prototype.fnPerimeter = function() {
+
+        return d([this.coordinates[0], this.coordinates[1], this.coordinates[2], this.coordinates[3]]) +
+            d([this.coordinates[2], this.coordinates[3], this.coordinates[4], this.coordinates[5]]) +
+            d([this.coordinates[4], this.coordinates[5], this.coordinates[6], this.coordinates[7]]) +
+            d([this.coordinates[6], this.coordinates[7], this.coordinates[0], this.coordinates[1]]);
+    }
+    // Triangle.prototype=new Shape();
+    // Quadrilateral.prototype=new Shape();
+    // Square.prototype=new Quadrilateral();
+
+// // inheritence should come before creating instances
+
+
+
+
+// //var shape=new Shape();
+var triangle = new Triangle("mytriangle", [0, 1, 2, 3, 4, 5]);
+// //var quadrilateral= new Quadrilateral();
+var square = new Square("mysquare", [0, 0, 1, 0, 1, 1, 0, 1]);
+
+console.log(triangle.coordinates);
 console.log(square)
-
-
-
-
-
-
-// Create a set of object types that describe a series of related objects that may share behavior and/or attributes. Code the example and another set of classes different from the example. Add properties/methods as needed.
-
-// Example:
-
-// Shape //{ pEdges, fnDisplay }
-// Quadrilateral is Shape //{ fnArea, fnPerimeter}
-// Square is Quadrilateral //{ }
-// Triangle is Shape //{fnArea, fnPerimeter}
-// Use of inheritance, prototype, and function overwriting.
+console.log(square.fnPerimeter());
+console.log(Quadrilateral)
