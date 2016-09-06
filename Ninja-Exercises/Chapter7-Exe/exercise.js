31e7a6adc8c6e1bbf2fde500b2919528b27d4920
ShirinKsquare <shirin.sardar@ksquarelabs.com>
2016-09-05 22:59:56 -0500

    Update exercise.js

--- Ninja-Exercises/Chapter7-Exe/exercise.js
+++ Ninja-Exercises/Chapter7-Exe/exercise.js
@@ -29,3 +29,32 @@ hextorgb("3322ff");
 //'rgb(48,32,255)'
 //'rgb(51,34,255)'
 //'rgb(51,34,255)'
+
+
+
+/* Exercise2 */
+function gregorian_to_jalali(USDate){
+  var result = /(\d{1,2})\/(\d{1,2})\/(\d{4})/.exec(USDate);
+  var gm = result[1];
+  var gd = result[2];
+  var gy = result[3];
+ g_d_m=[0,31,59,90,120,151,181,212,243,273,304,334];
+ jy=(gy<=1600)?0:979;
+ gy-=(gy<=1600)?621:1600;
+ gy2=(gm>2)?(gy+1):gy;
+ days=(365*gy) +(parseInt((gy2+3)/4)) -(parseInt((gy2+99)/100)) 
++(parseInt((gy2+399)/400)) -80 +gd +g_d_m[gm-1];
+ jy+=33*(parseInt(days/12053)); 
+ days%=12053;
+ jy+=4*(parseInt(days/1461));
+ days%=1461;
+ jy+=parseInt((days-1)/365);
+ if(days > 365)days=(days-1)%365;
+ jm=(days < 186)?1+parseInt(days/31):7+parseInt((days-186)/30);
+ jd=1+((days < 186)?(days%31):((days-186)%30));
+ return [jy,jm,jd];
+}
+
+
+gregorian_to_jalali("09/16/2014");
+// the year is nt working
